3661ada5fa9508ad467882748ad11b0ff855961d
Agata Jarmoszuk <agnes365@vp.pl>
2021-12-19 17:10:08 +0100

    Finish 9.4

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,57 @@
-import { settings, select } from './settings.js';
+import { settings, select, classNames } from './settings.js';
 import Product from './components/Product.js';
 import Cart from './components/Cart.js';
+import Booking from './components/Booking.js';
 
 const app = {
+  initPages: function () {
+    const thisApp = this;
+    thisApp.pages = document.querySelector(select.containerOf.pages).children;
+    thisApp.navLinks = document.querySelectorAll(select.nav.links);
+    const idFromHash = window.location.hash.replace('#/', '');
+
+    let pageMatchingHash = false;
+    for (let page of thisApp.pages) {
+      if (page.id == idFromHash) {
+        pageMatchingHash = page.id;
+        break;
+      }
+
+    }
+
+    thisApp.activatePage(pageMatchingHash);
+    for (let link of thisApp.navLinks) {
+      link.addEventListener('click', function (event) {
+        const clickedElement = this;
+        event.preventDefault();
+
+        /*get page id from href atribute */
+        const id = clickedElement.getAttribute('href').replace('#', '');
+        /* run thisApp.activatePage with that id*/
+        thisApp.activatePage(id);
+        /* change URL hash */
+        window.location.hash = '#/' + id;
+
+      });
+    }
+  },
+  activatePage: function (pageId) {
+    const thisApp = this;
+
+    /*add class "active" to matching pages, remove from non-machting */
+    for (let page of thisApp.pages) {
+      page.classList.toggle(classNames.pages.active, page.id == pageId);
+    }
+
+    /*add class "active" to matching links, remove from non-machting */
+    for (let link of thisApp.navLinks) {
+      link.classList.toggle(
+        classNames.nav.active,
+        link.getAttribute('href') == '#' + pageId
+      );
+    }
+  },
+
   initData: function () {
     const thisApp = this;
     thisApp.data = {};
@@ -23,6 +72,8 @@ const app = {
     const thisApp = this;
     thisApp.initData();
     thisApp.initCart();
+    thisApp.initPages();
+    thisApp.initBooking();
   },
   initMenu: function () {
     const thisApp = this;
@@ -38,6 +89,11 @@ const app = {
     thisApp.productList.addEventListener('add-to-cart', function (event) {
       app.cart.add(event.detail.product);
     });
+  },
+  initBooking: function () {
+    const thisApp = this;
+    const widgetBooking = document.querySelector(select.containerOf.booking);
+    thisApp.booking = new Booking(widgetBooking);
   }
 };
 
